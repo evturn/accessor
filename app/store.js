6faf325f638b4acaae3d1830e1b065570983fda6
Evan Turner <evturn@gmail.com>
2016-06-25 15:00:29 -0400

    Fix hot module replacement

--- app/store.js
+++ app/store.js
@@ -8,7 +8,7 @@ import createReducer from './reducers'
 const observableMiddleware = reduxObservable()
 const devtools = window.devToolsExtension || (() => noop => noop)
 
-export default function configureStore(initialState = {}, history) {
+export default function configureStore(initialState={}, history) {
   const middlewares = [
     observableMiddleware,
     routerMiddleware(history),
@@ -29,10 +29,17 @@ export default function configureStore(initialState = {}, history) {
     compose(...enhancers)
   )
 
+  store.asyncReducers = {}
+
   if (module.hot) {
-    module.hot.accept('./reducers', () => {
-      const nextRootReducer = require('./reducers').default
-      store.replaceReducer(nextRootReducer)
+    module.hot.accept('./reducers', _ => {
+      System.import('./reducers')
+        .then(reducerModule => {
+          const createReducers = reducerModule.default
+          const nextReducers = createReducers(store.asyncReducers)
+
+          store.replaceReducer(nextReducers)
+        })
     })
   }
 
