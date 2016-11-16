52be27547dfb8c3fbbe789334fa4c78ad32f6761
Evan Turner <evturn@gmail.com>
2016-11-16 12:03:09 -0500

    Create exit icon

--- app/containers/Header/index.js
+++ app/containers/Header/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import classNames from 'classnames/bind'
+import ExitIcon from 'components/Icons/ExitIcon'
 import * as Actions from 'api/actions'
 import css from './style.css'
 
 export const Header = ({ cx, user, signOut }) => {
   return (
-    <div className={css.header}>
+    <div className={css.root}>
       <div className={cx('title', {shrink: user})} />
       {user
-        ? <div className={css.logout} onClick={signOut} />
+        ? <div className={css.logout} onClick={signOut}><ExitIcon /></div>
         : null
       }
     </div>
